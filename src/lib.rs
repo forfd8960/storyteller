a66293715f719a3f13f6e8ea67082dfd764db8be
forfd8960 <forfd8960@gmail.com>
2024-09-26 22:44:23 +0800

    feat: AppConfig, AppState

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,39 @@
+use anyhow::Result;
+use sqlx::PgPool;
+use std::{ops::Deref, sync::Arc};
+
 pub mod errors;
 pub mod models;
 pub mod server;
 pub mod service;
+
+pub struct AppConfig {
+    port: u8,
+    db_url: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    inner: Arc<AppStateInner>,
+}
+
+#[derive(Debug, Clone)]
+pub struct AppStateInner {
+    pool: PgPool,
+}
+
+impl AppState {
+    pub async fn new(conf: &AppConfig) -> Result<Self> {
+        let pool = PgPool::connect(&conf.db_url).await?;
+        Ok(Self {
+            inner: Arc::new(AppStateInner { pool }),
+        })
+    }
+}
+
+impl Deref for AppState {
+    type Target = AppStateInner;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,28 +1,20 @@
-use crate::errors::AppError;
+use crate::{errors::AppError, AppState};
 use axum::{
     routing::{get, post},
     Router,
 };
-use serde::Serialize;
 
 pub mod story_service;
 pub mod user_service;
 
-#[derive(Debug, Serialize)]
-pub enum APIResponse {
-    OK,
-    User,
-    Story,
-    Message(String),
-}
-
 pub struct Server {}
 
-pub fn set_app_router() -> Result<Router, AppError> {
+pub fn set_app_router(state: AppState) -> Result<Router, AppError> {
     let routers = Router::new()
         .route("/users/register", post(user_service::register))
         .route("/users/login", post(user_service::login))
-        .route("/users/:id", get(user_service::get_user));
+        .route("/users/:id", get(user_service::get_user))
+        .with_state(state);
 
     Ok(routers)
 }
